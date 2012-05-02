b499a772c9ac0ddb5ea11587ce70f2ceff9fff29
Andriy Tomchuk <andrij.tomchuk@gmail.com>
2012-05-02 16:54:41 +0300

    Updated tests. To make more detailed test for Singleton Info.

--- njlTest/info/tests.py
+++ njlTest/info/tests.py
@@ -7,7 +7,7 @@ Replace this with more appropriate tests for your application.
 
 from django.test import TestCase
 
-from info.models import InfoSingleton
+from info.models import Info
 
 
 class SimpleTest(TestCase):
@@ -24,17 +24,18 @@ class TestInfoModel(TestCase):
         Tests info model. Tests also its Singleton property status(only one item should be created in the db)
         """
 
-        obj_0 = InfoSingleton.objects.create(name='Asdf', surname='Tom', date_of_birth="1983-11-02")
-        self.assertEqual(InfoSingleton.objects.count(), 1)
-        self.assertEqual(InfoSingleton.objects.all()[0].surname, 'Tom')
-        self.assertEqual(InfoSingleton.objects.all()[0].name, 'Asdf')
+        obj_0 = Info.objects.create(name='Asdf', surname='Tom', date_of_birth="1983-11-02")
+        obj_1 = Info.objects.create(name='Asd1', surname='T1m', date_of_birth="1983-11-02")
+        self.assertEqual(Info.objects.count(), 1)
+        self.assertEqual(Info.objects.all()[0].surname, 'T1m')
+        self.assertEqual(Info.objects.all()[0].name, 'Asd1')
 
-        obj = InfoSingleton.objects.create(name='Andriy', surname='Tomchuk', date_of_birth="1983-11-02", 
+        obj = Info.objects.create(name='Andriy', surname='Tomchuk', date_of_birth="1983-11-02", 
                                             bio='Some biographic info about myself',    
                                             email='njlytoh@gmail.com', jabber='njlytoh@gmail.com',
                                             skype='njlytoh', other_contacts='phone: +3809374736283')
         
-        self.assertEqual(InfoSingleton.objects.count(), 1)
-        self.assertEqual(InfoSingleton.objects.all()[0].name, obj.name)
-        self.assertEqual(InfoSingleton.objects.all()[0].other_contacts, obj.other_contacts)
+        self.assertEqual(Info.objects.count(), 1)
+        self.assertEqual(Info.objects.all()[0].name, obj.name)
+        self.assertEqual(Info.objects.all()[0].other_contacts, obj.other_contacts)
 
