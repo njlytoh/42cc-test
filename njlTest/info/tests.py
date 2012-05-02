9b35a02b6c4ed2baf6192c324181dcd500197cef
a <a@a-P50IJ.(none)>
2012-05-02 12:58:10 +0300

    Added first test for info InfoSingleton Model;

--- njlTest/info/tests.py
+++ njlTest/info/tests.py
@@ -7,6 +7,8 @@ Replace this with more appropriate tests for your application.
 
 from django.test import TestCase
 
+from info.models import InfoSingleton
+
 
 class SimpleTest(TestCase):
     def test_basic_addition(self):
@@ -14,3 +16,25 @@ class SimpleTest(TestCase):
         Tests that 1 + 1 always equals 2.
         """
         self.assertEqual(1 + 1, 2)
+
+class TestInfoModel(TestCase):
+    
+    def test_model(self):
+        """
+        Tests info model. Tests also its Singleton property status(only one item should be created in the db)
+        """
+
+        obj_0 = InfoSingleton.objects.create(name='Asdf', surname='Tom')
+        self.assertEqual(InfoSingleton.objects.count(), 1)
+        self.assertEqual(InfoSingleton.objects.all()[0].surname, 'Tom')
+        self.assertEqual(InfoSingleton.objects.all()[0].name, 'Asdf')
+
+        obj = InfoSingleton.objects.create(name='Andriy', surname='Tomchuk', date_of_birth='03.11.1983', 
+                                            bio='Some biographic info about myself',    
+                                            email='njlytoh@gmail.com', jabber='njlytoh@gmail.com',
+                                            skype='njlytoh', other_contacts='phone: +3809374736283')
+        
+        self.assertEqual(InfoSingleton.objects.count(), 1)
+        self.assertEqual(InfoSingleton.objects.all()[0].name, obj.name)
+        self.assertEqual(InfoSingleton.objects.all()[0].other_contacts, obj.other_contacts)
+
