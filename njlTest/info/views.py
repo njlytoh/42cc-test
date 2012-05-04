acb942ec661aa9e690f87a3c697b6c087afade71
Andriy Tomchuk <andrij.tomchuk@gmail.com>
2012-05-04 20:04:21 +0300

    added initial fixtures to info app. And first half-ready view

--- njlTest/info/views.py
+++ njlTest/info/views.py
@@ -1,1 +1,12 @@
 # Create your views here.
+
+from django.shortcuts import render_to_response
+
+from info.models import Info
+
+
+def info_view(request):
+    info = Info.objects.get()
+    return render_to_response('info/info.html', {'info': info} )
+
+
